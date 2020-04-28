2e7516c09c3e83803aafb265de246e3cbe8ea40f
ZJ Xue <blade1565@outlook.com>
2020-04-28 23:48:25 +0800

    2020-4-28 建立目录
    初始化当前项目，建好对应的文件，添加说明

---
+++ create_db.sql
@@ -0,0 +1,51 @@
+# 创建数据库
+create database sql50;
+
+# 学生表
+create table Student(SId varchar(10),Sname varchar(10),Sage datetime,Ssex varchar(10));
+insert into Student values('01' , '赵雷' , '1990-01-01' , '男');
+insert into Student values('02' , '钱电' , '1990-12-21' , '男');
+insert into Student values('03' , '孙风' , '1990-12-20' , '男');
+insert into Student values('04' , '李云' , '1990-12-06' , '男');
+insert into Student values('05' , '周梅' , '1991-12-01' , '女');
+insert into Student values('06' , '吴兰' , '1992-01-01' , '女');
+insert into Student values('07' , '郑竹' , '1989-01-01' , '女');
+insert into Student values('09' , '张三' , '2017-12-20' , '女');
+insert into Student values('10' , '李四' , '2017-12-25' , '女');
+insert into Student values('11' , '李四' , '2012-06-06' , '女');
+insert into Student values('12' , '赵六' , '2013-06-13' , '女');
+insert into Student values('13' , '孙七' , '2014-06-01' , '女');
+
+# 科目表
+create table Course(CId varchar(10),Cname nvarchar(10),TId varchar(10));
+insert into Course values('01' , '语文' , '02');
+insert into Course values('02' , '数学' , '01');
+insert into Course values('03' , '英语' , '03');
+
+# 教师表
+create table Teacher(TId varchar(10),Tname varchar(10));
+insert into Teacher values('01' , '张三');
+insert into Teacher values('02' , '李四');
+insert into Teacher values('03' , '王五');
+
+# 成绩表
+create table SC(SId varchar(10),CId varchar(10),score decimal(18,1));
+insert into SC values('01' , '01' , 80);
+insert into SC values('01' , '02' , 90);
+insert into SC values('01' , '03' , 99);
+insert into SC values('02' , '01' , 70);
+insert into SC values('02' , '02' , 60);
+insert into SC values('02' , '03' , 80);
+insert into SC values('03' , '01' , 80);
+insert into SC values('03' , '02' , 80);
+insert into SC values('03' , '03' , 80);
+insert into SC values('04' , '01' , 50);
+insert into SC values('04' , '02' , 30);
+insert into SC values('04' , '03' , 20);
+insert into SC values('05' , '01' , 76);
+insert into SC values('05' , '02' , 87);
+insert into SC values('06' , '01' , 31);
+insert into SC values('06' , '03' , 34);
+insert into SC values('07' , '02' , 89);
+insert into SC values('07' , '03' , 98);
+
